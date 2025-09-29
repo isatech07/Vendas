c19b74d612ce2bb3e09a1f07449890d5aaec5965
Tânia Isabelle Santana <isabelle.santana@aluno.ifsp.edu.br>
2025-09-29 16:24:43 -0300

    primeiros passos

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+import { Link } from 'react-router-dom'
+
+export default function Page() {
+  return (
+    <div>
+      <h2>Página Home</h2>
+      <Link to='/'>Home</Link>
+    </div>
+  )
+}

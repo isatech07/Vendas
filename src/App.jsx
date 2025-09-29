84b1bb59acec67f180f573ec2dec2713b2010f44
Tânia Isabelle Santana <isabelle.santana@aluno.ifsp.edu.br>
2025-09-29 17:43:58 -0300

    estilizando o header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes } from 'react-router-dom'
 import './App.css'
+import Routers from './Routers'
+import Header from './components/Header'
 
 function App() {
   return (
-    <>
-    </>
+    <div>
+      <Header />
+      <main>
+        <Routers />
+      </main>
+    </div>
   )
 }
 
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,12 +1,10 @@
-import {Link} from 'react-router-dom'
-
 import { Link } from 'react-router-dom'
 
 export default function Page() {
   return (
     <div>
-      <h2>Página Home</h2>
-      <Link to='/'>Home</Link>
+      <h2>Página Sobre</h2>
+      <Link to="/">Home</Link>
     </div>
   )
 }
